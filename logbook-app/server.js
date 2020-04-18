f397c669cc057f5082d2d5f794215f349f8285a1
Justin Hubbell <hubbsy@Justins-MacBook-Pro.local>
2020-04-18 15:44:52 -0600

    set up basic file structure and express server

---
+++ logbook-app/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//TEST ROUTE
+app.get('/api/hello', (req, res) => {
+  res.send({ express: 'Hello From the Lizard People!' });
+});
+
+
+
+
+app.listen(PORT, () => console.log(`listening on port: ${PORT} baby!`));
